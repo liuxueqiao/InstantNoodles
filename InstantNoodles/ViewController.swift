65195215006d38e1d7dcd1c34cb62fbf8ecdef66
liuxueqiao <2207766467@qq.com>
2020-07-31 18:18:25 +0800

    新建项目，引入pods

---
+++ InstantNoodles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstantNoodles
+//
+//  Created by apple on 2020/7/31.
+//  Copyright © 2020 qiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+target "InstantNoodles" do
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+inhibit_all_warnings!
+
+use_frameworks!
+
+    pod 'SnapKit'
+    
+end
